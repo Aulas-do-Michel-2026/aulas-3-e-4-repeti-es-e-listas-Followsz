0d2bbd9a9ba29ad519569902926fa694c21f2bb0
Wagner Lucas <96394353+Followsz@users.noreply.github.com>
2026-05-29 15:58:02 -0300

    Compare largest elements of two input lists

--- exercicio_4.py
+++ exercicio_4.py
@@ -30,7 +30,19 @@ Ambas
 """
 
 # Código para pegar as listas de input
+
+
+# Fazer a partir daqui
+
 primeira_lista = [*map(int, input("Digite a sua primeira lista (separando os números por vírgula): ").split(","))]
 segunda_lista = [*map(int, input("Digite a sua segunda lista (separando os números por vírgula): ").split(","))]
 
-# Fazer a partir daqui
+maior_primeira = max(primeira_lista)
+maior_segunda = max(segunda_lista)
+
+if maior_primeira > maior_segunda:
+    print("Primeira")
+elif maior_segunda > maior_primeira:
+    print("Segunda")
+else:
+    print("Ambas")
