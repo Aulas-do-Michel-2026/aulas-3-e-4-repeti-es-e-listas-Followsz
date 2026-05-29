8eeabb71691f8233b32771d513fc6914863b1691
Wagner Lucas <96394353+Followsz@users.noreply.github.com>
2026-05-29 15:54:15 -0300

    Update exercicio_3.py

--- exercicio_3.py
+++ exercicio_3.py
@@ -20,6 +20,14 @@ Os números ímpares são [1, 3, 5]
 """
 
 # Código para pegar a lista
-lista = [*map(int, input("Digite a sua lista (separando os números por vírgula): ").split(","))]
 
 # Fazer a partir daqui...
+
+lista = [*map(int, input("Digite a sua lista (separando os números por vírgula): ").split(","))]
+numeros_impares = []
+
+for numero in lista:
+    if numero % 2 != 0:
+        numeros_impares.append(numero) 
+
+print(f"Os números ímpares são {numeros_impares}")
